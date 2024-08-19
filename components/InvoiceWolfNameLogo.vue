0292786575d6eda28b37362f29fbb6815b9f9e65
Niek <frankenstein795@gmail.com>
2024-08-19 16:39:39 +0200

    Remove dev logo, custom navbar

--- components/InvoiceWolfNameLogo.vue
+++ components/InvoiceWolfNameLogo.vue
@@ -5,8 +5,6 @@ defineProps<{
 	free?: boolean;
 	noLogo?: boolean;
 }>();
-
-const devMode = process.env.NODE_ENV !== "production";
 </script>
 
 <template>
@@ -17,12 +15,6 @@ const devMode = process.env.NODE_ENV !== "production";
 				<span class="select-none text-xl font-bold">Invoice<span
 					class="text-primary-500"
 				>Wolf <span v-if="free" class="align-bottom text-xs">CUB</span></span></span>
-				<small
-					v-if="devMode"
-					class="select-none text-center text-[11px] font-bold text-danger"
-				>
-					DEVELOPMENT
-				</small>
 			</div>
 		</div>
 		<div v-else-if="size === 'large'" class="flex flex-row items-center gap-2">
@@ -31,12 +23,6 @@ const devMode = process.env.NODE_ENV !== "production";
 				<span class="select-none text-5xl font-bold">Invoice<span
 					class="text-primary-500"
 				>Wolf <span v-if="free" class="align-bottom text-lg">CUB</span></span></span>
-				<small
-					v-if="devMode"
-					class="select-none text-center font-bold text-danger"
-				>
-					DEVELOPMENT
-				</small>
 			</div>
 		</div>
 	</div>
@@ -47,12 +33,6 @@ const devMode = process.env.NODE_ENV !== "production";
 				<span class="select-none text-4xl font-bold">Invoice<span
 					class="text-primary-500"
 				>Wolf <span v-if="free" class="align-bottom text-xs">CUB</span></span></span>
-				<small
-					v-if="devMode"
-					class="select-none text-center font-bold text-danger"
-				>
-					DEVELOPMENT
-				</small>
 			</div>
 		</div>
 		<div v-else-if="size === 'large'" class="flex flex-col items-center gap-2">
@@ -61,12 +41,6 @@ const devMode = process.env.NODE_ENV !== "production";
 				<span class="select-none text-6xl font-bold">Invoice<span
 					class="text-primary-500"
 				>Wolf <span v-if="free" class="align-bottom text-xs">CUB</span></span></span>
-				<small
-					v-if="devMode"
-					class="select-none text-center text-xl font-bold text-danger"
-				>
-					DEVELOPMENT
-				</small>
 			</div>
 		</div>
 	</div>
