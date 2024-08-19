b0700e910e66e8424782f032592b1040d701c3cf
Niek <frankenstein795@gmail.com>
2024-08-19 17:55:17 +0200

    Set icon size using inline styles because tailwind is broken

--- components/FeatureCard.vue
+++ components/FeatureCard.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
 	<li class="flex w-full flex-col justify-center gap-4 rounded-xl border-2 border-primary-200 bg-primary-50 p-4 shadow-lg shadow-primary-100 dark:border-primary-700 dark:bg-primary-950 dark:shadow-primary-950">
-		<i :class="`pi pi-${icon} text-4xl`" />
+		<i :class="`pi pi-${icon}`" style="font-size: 2.25rem; line-height: 2.5rem" />
 		<h2 class="text-xl font-semibold">
 			<slot name="title" />
 		</h2>
