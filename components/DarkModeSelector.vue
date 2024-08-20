fa0835494f62b0e35edd4713801307c8e088dfa8
Niek <frankenstein795@gmail.com>
2024-08-20 09:27:59 +0200

    Redo DarkModeSelector.vue

--- components/DarkModeSelector.vue
+++ components/DarkModeSelector.vue
@@ -1,26 +1,12 @@
 <script setup lang="ts">
 const colorMode = useColorMode();
-
-function toggleColorMode(val: boolean) {
-	if (val) {
-		colorMode.preference = "dark";
-	}
-	else {
-		colorMode.preference = "light";
-	}
-}
 </script>
 
 <template>
-	<div class="flex flex-row items-center gap-2">
-		<i class="pi pi-sun" />
-		<pv-toggle-switch
-			:model-value="colorMode.value === 'dark'"
-			binary
-			@update:model-value="(val) => toggleColorMode(val)"
-		/>
-		<i class="pi pi-moon" />
-	</div>
+	<span class="cursor-pointer text-primary-300 duration-300 hover:text-primary-400 dark:text-primary-500">
+		<i v-if="colorMode.value === 'light'" class="pi pi-moon" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'dark'" />
+		<i v-if="colorMode.value === 'dark'" class="pi pi-sun" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'light'" />
+	</span>
 </template>
 
 <style scoped>
