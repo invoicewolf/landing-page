91c3eccc6b342c2c24243cb07360240f4653145e
Niek <frankenstein795@gmail.com>
2024-08-20 18:25:48 +0200

    Remove fallback because it is too intrusive

--- components/DarkModeSelector.vue
+++ components/DarkModeSelector.vue
@@ -8,10 +8,5 @@ const colorMode = useColorMode();
 			<i v-if="colorMode.value === 'light'" class="pi pi-moon" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'dark'" />
 			<i v-else-if="colorMode.value === 'dark'" class="pi pi-sun" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'light'" />
 		</span>
-		<template #fallback>
-			<span class="cursor-pointer text-primary-300 duration-300 hover:text-primary-400 dark:text-primary-500">
-				<i class="pi pi-spinner-dotted" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'dark'" />
-			</span>
-		</template>
 	</ClientOnly>
 </template>
