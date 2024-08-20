9ae8d1166e8f19fe9d53dea7358c30d956222153
Niek <frankenstein795@gmail.com>
2024-08-20 20:30:16 +0200

    Use functions instead of watcher

--- components/DarkModeSelector.vue
+++ components/DarkModeSelector.vue
@@ -7,16 +7,22 @@ if (cookie.value) {
 	colorMode.preference = cookie.value;
 }
 
-watch(colorMode, () => {
-	cookie.value = colorMode.value;
-});
+function setDark() {
+	cookie.value = "dark";
+	colorMode.preference = "dark";
+}
+
+function setLight() {
+	cookie.value = "light";
+	colorMode.preference = "light";
+}
 </script>
 
 <template>
 	<ClientOnly>
 		<span class="cursor-pointer text-primary-300 duration-300 hover:text-primary-400 dark:text-primary-500">
-			<i v-if="colorMode.value === 'light'" class="pi pi-moon" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'dark'" />
-			<i v-else-if="colorMode.value === 'dark'" class="pi pi-sun" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'light'" />
+			<i v-if="colorMode.value === 'light'" class="pi pi-moon" style="font-size: 1.25rem; line-height: 1.75rem" @click="setDark" />
+			<i v-else-if="colorMode.value === 'dark'" class="pi pi-sun" style="font-size: 1.25rem; line-height: 1.75rem" @click="setLight" />
 		</span>
 	</ClientOnly>
 </template>
