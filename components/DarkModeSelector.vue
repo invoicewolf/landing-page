3cb22a1c72d42825eb8315fe8381f12d9bd9b1f3
Niek <frankenstein795@gmail.com>
2024-08-20 10:48:57 +0200

    Revert changes

--- components/DarkModeSelector.vue
+++ components/DarkModeSelector.vue
@@ -3,15 +3,8 @@ const colorMode = useColorMode();
 </script>
 
 <template>
-	<ClientOnly>
-		<span class="cursor-pointer text-primary-300 duration-300 hover:text-primary-400 dark:text-primary-500">
-			<i v-if="colorMode.value === 'light'" class="pi pi-moon" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'dark'" />
-			<i v-else-if="colorMode.value === 'dark'" class="pi pi-sun" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'light'" />
-		</span>
-		<template #fallback>
-			<span class="cursor-pointer text-primary-300 duration-300 hover:text-primary-400 dark:text-primary-500">
-				<i class="pi pi-spinner-dotted" style="font-size: 1.25rem; line-height: 1.75rem" />
-			</span>
-		</template>
-	</ClientOnly>
+	<span class="cursor-pointer text-primary-300 duration-300 hover:text-primary-400 dark:text-primary-500">
+		<i v-if="colorMode.value === 'light'" class="pi pi-moon" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'dark'" />
+		<i v-else-if="colorMode.value === 'dark'" class="pi pi-sun" style="font-size: 1.25rem; line-height: 1.75rem" @click="colorMode.preference = 'light'" />
+	</span>
 </template>
